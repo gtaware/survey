bee8bd975fc3a9139e5d5288b852f6c3bddbfb59
gtaware <inform.taware@gmail.com>
2015-02-10 15:15:26 +0100

    Commit: many new files. Previous command (git add *) get error that the paths are ignored by one of your .gitignore files: bin and vendor. So use -f if you want to add them. (git add -f *) and then this meesahe (git commit -m 'commit message')

---
+++ src/CeisSurvey/Bundle/UsersBundle/Form/Type/UserType.php
@@ -0,0 +1,61 @@
+<?php
+
+// src/AppBundle/Form/Type/TaskType.php
+
+namespace CeisSurvey\Bundle\UsersBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType {
+    
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        $resolver->setDefaults(array(
+               'validation_groups' => array('registration'),
+        ));
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('fname', 'text', array(
+                'attr' => array('maxlength' => 20)
+            ))
+            ->add('lname', 'text', array(
+                'attr' => array('maxlength' => 20)
+            ))
+            ->add('gender', 'choice', array(
+                'choices' => array(
+                    'Male' => 'Male',
+                    'Female' => 'Female',
+                    'Others' => 'Others',
+                    ),
+            ))
+            ->add('function', 'text', array(
+                'attr' => array('maxlength' => 50)
+            ))
+            ->add('email', 'text', array(
+                'attr' => array('maxlength' => 50)
+            ))
+            ->add('username', 'text', array(
+                'attr' => array('maxlength' => 10)
+            ))
+            ->add('password', 'password',array(
+                'attr' => array(
+                                'maxlength' => 10,
+                                ),
+                ))
+            ->add('Sign in', 'submit', array(
+                    'attr' => array(
+                                'class' => 'btn btn-success',
+                     )
+                    ));
+    }
+
+    public function getName() {
+        return 'user';
+    }
+
+}
+
+?>
